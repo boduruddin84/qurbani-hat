79429c228a6b31f01202923339b144ab386aa43f
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-04-30 10:08:10 +0600

    Navbar completed

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,45 +1,55 @@
 "use client";
-import Link from "next/link";
-import { useAuth } from "@/context/AuthContext";
+import Link from "next/link"
+import { useState } from "react";
+import { FaCow } from "react-icons/fa6"
+import { IoMenu } from "react-icons/io5";
 
-export default function Navbar() {
-  const { user } = useAuth();
 
+const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
   return (
-    <div className="bg-white shadow-md sticky top-0 z-50">
+    <div>
+      <div className="bg-white shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 flex justify-between items-center h-16">
 
-        {/* Logo */}
-        <Link href="/" className="text-xl font-bold text-green-600">
-          🐄 QurbaniHat
+        
+        <Link href="/" className="flex justify-between p-4">
+          <h1 className="flex justify-center items-center gap-2 text-xl font-bold text-green-600"><FaCow /> <span className="hidden md:flex">QurbaniHat</span></h1>
+
+          <button onClick={() => setOpen(!open)} className="m-2 md:hidden">
+          <IoMenu />
+        </button>
         </Link>
 
-        {/* Links */}
+        
         <div className="hidden md:flex gap-6">
           <Link href="/">Home</Link>
           <Link href="/animals">All Animals</Link>
         </div>
 
-        {/* Auth */}
+        {open && (
+        <div className="flex flex-col p-4 gap-3 md:hidden">
+          <Link href="/">Home</Link>
+          <Link href="/animals">All Animals</Link>
+        </div>
+      )}
+
+        
         <div>
-          {user ? (
-            <div className="flex items-center gap-3">
-              <img
-                src={user.photoURL}
-                className="w-8 h-8 rounded-full"
-              />
-              <Link href="/my-profile">Profile</Link>
-            </div>
-          ) : (
+          
             <div className="flex gap-2">
               <Link href="/login" className="btn btn-sm">Login</Link>
               <Link href="/register" className="btn btn-sm btn-success">
                 Register
               </Link>
             </div>
-          )}
+         
         </div>
       </div>
     </div>
-  );
+    </div>
+  )
 }
+
+export default Navbar
