0e94f401bdc8fb6569bd09a6780d3a682af797ab
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-05-04 23:08:37 +0600

    Booking form responsive updated

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -21,7 +21,7 @@ const BookingForm = () => {
   return (
     <div className="w-80 md:w-96 mx-auto">
         <h3 className="text-2xl font-semibold mb-5">Fill the form for booking</h3>
-      <Form className="mb-10 flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+      <Form className="mb-10 flex w-70 md:w-80 mx-auto flex-col gap-4" onSubmit={onSubmit}>
         <TextField isRequired name="name" type="name">
           <Label>Name</Label>
           <Input placeholder="Enter your name" />
